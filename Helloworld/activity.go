38c8ced60480a47b25229de089ca1c263d297877
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-03-28 15:11:49 +0530

    first commit

---
+++ Helloworld/activity.go
@@ -0,0 +1,35 @@
+package Helloworld
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-helloworld")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// do eval
+	name := context.GetInput("name").(string)
+	salutation := context.GetInput("salutation").(string)
+
+	log.Debug("The flogo engine says [%s] to [%s]", salutation, name)
+	context.SetOutput("result", "The flogo engine says"+salutation+" to "+name)
+	return true, nil
+}
