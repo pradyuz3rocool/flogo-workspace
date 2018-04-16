0b7107c1e4408bd1a4943d3eed8191b8f9e244b1
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-04-16 16:34:05 +0530

    updates the sas token generation

--- activity/azureiot/ioHubhttpClient.go
+++ activity/azureiot/ioHubhttpClient.go
@@ -158,9 +158,10 @@ func (c *IotHubHTTPClient) buildSasToken(uri string) string {
 
 	if c.sharedAccessKeyName != "" {
 		return fmt.Sprintf("SharedAccessSignature sig=%s&se=%d&skn=%s&sr=%s", encodedSignature, timestamp, c.sharedAccessKeyName, encodedURI)
+	} else {
+		return fmt.Sprintf("SharedAccessSignature sig=%s&se=%d&sr=%s", encodedSignature, timestamp, encodedURI)
 	}
 
-	return fmt.Sprintf("SharedAccessSignature sig=%s&se=%d&sr=%s", encodedSignature, timestamp, encodedURI)
 }
 
 func (c *IotHubHTTPClient) performRequest(method string, uri string, data string) (string, string) {
