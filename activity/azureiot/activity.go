686435ea1c37fd413b3b6c58a0af0fb9d4bebf1f
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-04-17 13:49:47 +0530

    latest code

--- activity/azureiot/activity.go
+++ activity/azureiot/activity.go
@@ -48,12 +48,11 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 
 	log.Debug("The connection string to device is [%s]", connectionString)
 	hostName, sharedAccessKey, sharedAccessKeyName, deviceID, err := parseConnectionString(connectionString)
-	log.Debug("sharedAccessKeyName", sharedAccessKeyName)
-	log.Debug("sharedAccessKey ", sharedAccessKey)
-	url := fmt.Sprintf("https://%s/devices/%s/messages/deviceBound?api-version=2016-11-14", hostName, deviceID)
-	SaS := createSharedAccessToken(url, sharedAccessKeyName)
+	log.Info("sharedAccessKeyName", sharedAccessKeyName)
+	url := fmt.Sprintf("%s/devices/%s/messages/deviceBound?api-version=2016-11-14", hostName, deviceID)
+	SaS := createSharedAccessToken(url, sharedAccessKeyName, sharedAccessKey)
 
-	context.SetOutput(ovURL, url)
+	context.SetOutput(ovURL, "https://"+url)
 	context.SetOutput(ovToken, SaS)
 	return true, nil
 }
@@ -84,9 +83,9 @@ func tryGetKeyByName(v url.Values, key string) string {
 	return strings.Replace(v[key][0], " ", "+", -1)
 }
 
-func createSharedAccessToken(uri string, saKey string) string {
+func createSharedAccessToken(uri string, saName string, saKey string) string {
 
-	if len(uri) == 0 || len(saKey) == 0 {
+	if len(uri) == 0 || len(saName) == 0 || len(saKey) == 0 {
 		return "Missing required parameter"
 	}
 
@@ -99,6 +98,7 @@ func createSharedAccessToken(uri string, saKey string) string {
 	hmac := hmac.New(sha256.New, key)
 	hmac.Write([]byte(signature))
 	hmacString := template.URLQueryEscaper(base64.StdEncoding.EncodeToString(hmac.Sum(nil)))
-	result := "SharedAccessSignature sr=" + encoded + "&sig=" + hmacString + "&se=" + strconv.Itoa(int(ts))
+
+	result := "SharedAccessSignature sr=" + encoded + "&sig=" + hmacString + "&se=" + strconv.Itoa(int(ts)) + "&skn=" + saName
 	return result
 }
