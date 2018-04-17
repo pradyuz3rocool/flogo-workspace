d5c2680e1f45c4b794d940b59d52048ae677a04f
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-04-17 13:09:03 +0530

    Latest code

--- activity/azureiot/activity.go
+++ activity/azureiot/activity.go
@@ -49,6 +49,7 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	log.Debug("The connection string to device is [%s]", connectionString)
 	hostName, sharedAccessKey, sharedAccessKeyName, deviceID, err := parseConnectionString(connectionString)
 	log.Debug("sharedAccessKeyName", sharedAccessKeyName)
+	log.Debug("sharedAccessKey ", sharedAccessKey)
 	url := fmt.Sprintf("https://%s/devices/%s/messages/deviceBound?api-version=2016-11-14", hostName, deviceID)
 	SaS := createSharedAccessToken(url, sharedAccessKey)
 
