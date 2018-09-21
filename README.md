580bd31b75ebdd920ddb574b35d93ab915f3c189
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-09-20 22:10:47 -0700

    Updates the readme to the latest code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flogo-workspace
+
+This contains the flogo related actvities in general like activities, triggers and applications and other contriutions
+
+### Author
+
---
+++ apps/tempsense/src/tempsense/imports.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+
+	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
+	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
+	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+
+)
