686336871f228a2d9373592ed3972ba85481dd54
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-12-05 09:55:08 +0530

    formats the json file for dht.json file

--- apps/dhtxx/src/dhtxx/imports.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-
-	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/timer"
-	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
-	_ "github.com/pradyuz3rocool/flogo-workspace/activity/dhtxx"
-
-)
