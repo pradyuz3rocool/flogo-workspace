a35dbc2e39d077e3f6be77fff328cf8468b0f9e4
Pradyumna <pradyumna.chakicherla@isteer.com>
2018-12-14 15:50:38 +0530

    creates a ew trigger to start data after reading from mqtt

---
+++ trigger/dht11_read/README.md
@@ -0,0 +1,2 @@
+
+# 	dht11_read - Trigger
---
+++ trigger/dht11_read/trigger.go
@@ -0,0 +1,49 @@
+package dht11_read
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
+)
+
+// MyTriggerFactory My Trigger factory
+type MyTriggerFactory struct{
+	metadata *trigger.Metadata
+}
+
+// NewFactory create a new Trigger factory
+func NewFactory(md *trigger.Metadata) trigger.Factory {
+	return &MyTriggerFactory{metadata:md}
+}
+
+// New Creates a new trigger instance for a given id
+func (t *MyTriggerFactory) New(config *trigger.Config) trigger.Trigger {
+	return &MyTrigger{metadata: t.metadata, config:config}
+}
+
+// MyTrigger is a stub for your Trigger implementation
+type MyTrigger struct {
+	metadata *trigger.Metadata
+	config   *trigger.Config
+}
+
+// Initialize implements trigger.Init.Initialize
+func (t *MyTrigger) Initialize(ctx trigger.InitContext) error {
+	return nil
+}
+
+// Metadata implements trigger.Trigger.Metadata
+func (t *MyTrigger) Metadata() *trigger.Metadata {
+	return t.metadata
+}
+
+// Start implements trigger.Trigger.Start
+func (t *MyTrigger) Start() error {
+	// start the trigger
+	return nil
+}
+
+// Stop implements trigger.Trigger.Start
+func (t *MyTrigger) Stop() error {
+	// stop the trigger
+	return nil
+}
